0242274bf5fc5f5ed87da5d3cb2f5036af36a253
misbah-alvariz <81959331+misbah-alvariz@users.noreply.github.com>
2021-06-02 23:43:07 +0700

    Push index.php FIX

---
+++ assets/php/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="../img/logo.jpg">
+    <title>tawalap online</title>
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600" rel="stylesheet">
+    <link rel="stylesheet" href="../css/styleIndex.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="https://unpkg.com/animejs@3.0.1/lib/anime.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
+</head>
+
+<body class="is-boxed has-animations">
+    <div class="body-wrap" style="background-color:#2e617e">
+        <header class="site-header">
+            <div class="container">
+                <div class="site-header-inner">
+                    <div class="brand header-brand">
+                        <h1 class="m-0" style="color: white;"><img src="../img/logo3.png" style="float:left;" height="50" width="50">tawalap</h1>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <section class="hero">
+                <div class="container">
+                    <div class="hero-inner">
+                        <div class="hero-copy">
+                            <p class="text-justify" style="color: white;">Tawalap atau singkatan dari Tamu Wajib Lapor
+                                adalah sebuah Website yang dirancang untuk memudahkan warga dalam melakukan pelaporan
+                                1x24 jam</p>
+                            <div class="hero-cta">
+                                <a class="button" style="background-color: rgb(0, 119, 255); border-radius: 12px;" href="../php/login.php"><i class="fa fa-user"></i>&nbsp&nbspMasuk</a>
+                                <br />
+                                <br />
+                                <a class="button" style="background-color: rgb(0, 119, 255); border-radius: 12px;" href="../php/daftar.php"><i class="fa fa-user-plus"></i>&nbsp&nbspDaftar</a>
+                                <br />
+                                <br />
+                                <a class="button" style="background-color: rgb(0, 119, 255); border-radius: 12px;" href="../php/Bantuan.php"><i class="fa fa-info"></i>&nbsp&nbspBantuan</a>
+                            </div>
+                        </div>
+                        <div class="hero-figure anime-element">
+                            <svg class="placeholder" width="528" height="396" viewBox="0 0 528 396"></svg>
+                            <div class="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
+                            <div class="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
+                            <div class="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
+                            <div class="hero-figure-box hero-figure-box-04" data-rotation="-135deg"></div>
+                            <div class="hero-figure-box hero-figure-box-05" style="background-image: url('../img/Index1.png'); background-size: cover;"></div>
+                            <div class="hero-figure-box hero-figure-box-06" style="background-image: url('../img/Index2.png'); background-size: cover;"></div>
+                            <div class="hero-figure-box hero-figure-box-07" style="background-image: url('../img/Index3.png'); background-size: cover;"></div>
+                            <div class="hero-figure-box hero-figure-box-08" data-rotation="-22deg"></div>
+                            <div class="hero-figure-box hero-figure-box-09" data-rotation="-52deg"></div>
+                            <div class="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <script src="../js/mainIndex.js"></script>
+</body>
+
+</html>
