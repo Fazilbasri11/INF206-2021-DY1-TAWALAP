f6fa38508d2c30ef13aae54915d7d9c6dda45d39
Zulfanabila5 <82099241+Zulfanabila5@users.noreply.github.com>
2021-06-03 17:28:54 +0700

    Mengupload foto user, home dan bantuan

---
+++ assets/php/Bantuan.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ===== BOX ICONS ===== -->
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- ===== CSS ===== -->
+    <link rel="stylesheet" href="../css/StyleProsedur.css">
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="../img/logo.jpg" />
+
+    <title>TAWALAP | Bantuan</title>
+</head>
+
+<body id="body-pd">
+    <header class="header" id="header">
+        <div class="header__toggle">
+
+            <span class="nav__logo-name">TAWALAP online</span>
+        </div>
+    </header>
+    <br>
+
+    <a class="btn btn-info" href="../php/index.php"><i class="fa fa-reply" aria-hidden="true"></i>&nbsp&nbspKembali</a>
+
+    <br>
+    <br>
+    <div class="card text-center" style="border-color: white; border-width: 5px; border-radius: 12px;">
+        <div class="card-header" style="background-color: #2e617e; border-color: white; border-width: 2px;">
+            <h2 style="color: white;">Bantuan</h2>
+        </div>
+        <div class="card-body" style="background-color: #5995b3;">
+            <h5 class="card-title" style="color: white;"><b>Jika Anda Mengalami Masalah dapat menghubungi Admin Melalui Email Berikut</b></h5>
+            <p class="card-text" style="color: white;">Admin@gmail.com</p>
+            <h5 class="card-text" style="color: white;"><b>Atau Bisa Hubungi Admin Langsung dengan Menekan Tombol Dibawah</b></h5>
+            <a class="btn btn-info" href="http://wa.me/6282246261899?">Kesini</a>
+
+        </div>
+
+    </div>
+
+    <!--===== MAIN JS =====-->
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
