86949b3cb9774dd2abf7b36e1ba9f24fb87da410
Waliammursyidaa <waliam@mhs.unsyiah.ac.id>
2021-06-03 16:13:54 +0700

    push fix

--- assets/php/Foto_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'functions.php';
-if (isset($_GET['nik'])) {
-    $query = mysqli_query($conn, "select * from tamu where nik='" . $_GET['nik'] . "'");
-    $row = mysqli_fetch_array($query);
-    echo $row["foto_anda"];
-} else {
-    header('location:LaporanBaru.php');
-}
